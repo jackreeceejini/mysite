6df4fa3ec00a42668aaadad0a4559142c32a69fb
Jackreece Ejini <jackreeceejini@gmail.com>
2022-02-22 15:57:01 -0500

    completed send email

--- blog/views.py
+++ blog/views.py
@@ -51,7 +51,7 @@ def post_share(request, post_id):
                 f"{post.title}"
             message = f"Read {post.title} at {post_url}\n\n" \
                 f"{cd['name']}\'s comments: {cd['comments']}"
-            send_mail(subject, message, 'admin@myblog.com', cd['to']])
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
             sent = True
     else:
         form = EmailPostForm()
