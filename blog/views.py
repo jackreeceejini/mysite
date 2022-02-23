54ccf57a02b95fca4f9c4f29581c6f96971e2a25
Jackreece Ejini <jackreeceejini@gmail.com>
2022-02-23 15:55:36 -0500

    added comments

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
 from re import template
 from django.shortcuts import render, get_object_or_404 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import Post 
+from .models import Post, Comment
 from django.views.generic import ListView
 from django.core.mail import send_mail 
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -30,7 +30,27 @@ def post_list(request):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,publish__month=month,publish__day=day)
 
-    return render(request, 'blog/post/detail.html',{'post':post})
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+
+    new_comment = None 
+
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post 
+            #save the comment to the datebase 
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'blog/post/detail.html',{'post':post,
+    'comments':comments,
+    'new_comment':new_comment,
+    'comment_form':comment_form})
 
 
 
