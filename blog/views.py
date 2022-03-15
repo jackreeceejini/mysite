1a159d45924a3dc27b5c64a01e76895322df8e1e
Jackreece Ejini <jackreeceejini@gmail.com>
2022-03-15 16:38:59 -0400

    added similarity searching

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .models import Post, Comment
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 from .forms import EmailPostForm, CommentForm, SearchForm 
 from taggit.models import Tag 
 class PostListView(ListView):
@@ -73,9 +73,8 @@ def post_search(request):
                 SearchVector('body', weight='B')
             search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=search_vector,
-                rank=SearchRank(search_vector, search_query)
-            ).filter(rank__gte=0.3).order_by('-rank')
+                similarity=TrigramSimilarity('title',query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
     return render(request, 'blog/post/search.html', {
         'form':form,
         'query':query,
